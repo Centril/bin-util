ddc63ce9097331ee8cc667b93f36e4c496307dcb
Mazdak Farrokhzad <twingoow@gmail.com>
2015-09-21 09:57:25 +0200

    improved gca

--- gc.sh
+++ gc.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
 
-if [ ${1:0:1} == "-" ]
-then
-	git commit $@
-else
-	first="$1"
+msg=0
+if [ ${1:0:1} != "-" ] ; then
+	msg="$1"
 	shift
-	git commit -m "$first" $@
+fi
+
+push=0
+ARGS=()
+for var in "$@"
+do
+	if [[ $var == '-ps' ]] ; then
+		push=1
+	else
+		ARGS+=($var)
+	fi
+done
+
+if [ $msg -eq 0 ] ; then
+	git commit ${ARGS[*]}
+else
+	git commit -m "$msg" ${ARGS[*]}
+fi
+
+if [ $push -eq 1 ] ; then
+	git push
 fi
