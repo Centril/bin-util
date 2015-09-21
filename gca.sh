fd8681feaa4fa3c5093a6f5beaa6524400cabaca
Mazdak Farrokhzad <twingoow@gmail.com>
2015-09-21 09:57:25 +0200

    improved gca

--- gca.sh
+++ gca.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
-add="$1"
-msg="$2"
+add=$1
+msg=$2
 shift
 shift
 
+push=0
+ARGS=()
+for var in "$@"
+do
+	if [[ $var == '-ps' ]] ; then
+		push=1
+	else
+		ARGS+=($var)
+	fi
+done
+
 git add -A $add
 git status -sb
-git commit -m "$msg" $@
+git commit -m "$msg" ${ARGS[*]}
+
+if [ $push -eq 1 ] ; then
+	git push
+fi
